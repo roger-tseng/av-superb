b51c9d2c7b4840183d59d57aa759e27c47e2b9e5
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-04-06 22:38:45 +0800

    basic kinetics downstream

---
+++ downstream_tasks/kinetics/dataset.py
@@ -0,0 +1,67 @@
+import random
+
+import torch
+import torch.nn as nn
+from torch.utils.data.dataset import Dataset
+import torchaudio
+
+import csv
+import os
+from torchaudio.transforms import Resample
+
+
+SAMPLE_RATE = 16000
+
+class KineticsDataset(Dataset):
+    def __init__(self, mode, kinetics_root, **kwargs):
+        self.kinetics_root = kinetics_root
+        self.mode = mode
+        
+        if mode == "train":
+            self.path = kwargs["train_meta_location"]
+        elif mode == "validation":
+            self.path = kwargs["val_meta_location"]
+        elif mode == "test":
+            self.path = kwargs["test_meta_location"]
+        print("dataset meta path", self.path)
+
+        file = open(self.path, "r")
+        data = list(csv.reader(file, delimiter=","))
+        file.close()
+        print("data example", data[0])
+
+        self.dataset = data 
+        self.class_num = 700 
+
+
+
+
+    def __getitem__(self, idx):
+        audio_path = os.path.join(self.kinetics_root, self.dataset[idx][0])
+
+        wav, sr = torchaudio.load(audio_path)
+        # print(wav.shape) 
+        resampler = Resample(sr, SAMPLE_RATE)
+        wav = resampler(wav).mean(dim=0).squeeze(0)
+
+        # print(self.dataset[idx][0], "wav length", len(wav))
+        
+        label = int(self.dataset[idx][1]) 
+
+        # print(audio_path, label)
+
+        return wav, label
+
+
+
+    def __len__(self):
+        return len(self.dataset) 
+
+
+
+    def collate_fn(self, samples):
+        wavs, labels = [], []
+        for wav, label in samples:
+            wavs.append(wav)
+            labels.append(label)
+        return wavs, labels
---
+++ downstream_tasks/kinetics/model.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self, input_dim, hidden_dim, hidden_layers , output_class_num, **kwargs):
+        super(Model, self).__init__()
+
+        self.dropout = 0.45
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, hidden_layers, dropout = self.dropout, bidirectional = True, batch_first = True)
+
+        self.fc = nn.Sequential(
+            nn.Dropout(self.dropout),
+            nn.Linear(hidden_dim * 2, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(hidden_dim, output_class_num)
+        )
+
+    def forward(self, features):
+
+        out, _ = self.lstm(features)
+
+        out = out[:, -1, :]
+
+        predicted = self.fc(out)
+
+        return predicted
+
+# class Model(nn.Module):
+#     def __init__(self, input_dim, output_class_num, **kwargs):
+#         super(Model, self).__init__()
+#         self.linear = nn.Linear(input_dim, output_class_num)
+# 
+#     def forward(self, features):
+#         pooled = features.mean(dim=1)
+#         predicted = self.linear(pooled)
+#         return predicted
