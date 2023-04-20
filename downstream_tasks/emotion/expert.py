73cae3ba5febeaf2e1815f3b6b4db511c225fae3
ytc1002 <ytc1002@gmail.com>
2023-04-21 03:13:16 +0800

    update

--- downstream_tasks/emotion/expert.py
+++ downstream_tasks/emotion/expert.py
@@ -1,18 +1,20 @@
-import os
+"""
+Custom class for training/testing code for downstream audio-visual tasks
+Modified from https://github.com/s3prl/s3prl/blob/main/s3prl/downstream/example/expert.py
+"""
 import math
-import torch
+import os
 import random
 from pathlib import Path
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, random_split, DistributedSampler
 from torch.distributed import is_initialized
 from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, DistributedSampler, random_split
 
-from ..model import *
-from .model import *
 from .dataset import IEMOCAPDataset, collate_fn
+from .model import Model
 
 
 class DownstreamExpert(nn.Module):
@@ -22,99 +24,149 @@ class DownstreamExpert(nn.Module):
     """
 
     def __init__(self, upstream_dim, downstream_expert, expdir, **kwargs):
+        """
+        Args:
+            upstream_dim: int
+                Different upstream models will give different representation dimension
+                You might want to first project them to the same dimension
+            downstream_expert: dict
+                The 'downstream_expert' field specified in your downstream config file
+                eg. downstream/example/config.yaml
+            expdir: string
+                The expdir from command-line argument, you should save all results into
+                this directory, like some logging files.
+            **kwargs: dict
+                All the arguments specified by the argparser in run_downstream.py
+                and all the other fields in config.yaml, in case you need it.
+                Note. Feel free to add new argument for __init__ as long as it is
+                a command-line argument or a config field. You can check the constructor
+                code in downstream/runner.py
+        """
+
         super(DownstreamExpert, self).__init__()
         self.upstream_dim = upstream_dim
-        self.datarc = downstream_expert['datarc']
-        self.modelrc = downstream_expert['modelrc']
-
-        DATA_ROOT = self.datarc['root']
+        self.datarc = downstream_expert["datarc"]  # config for dataset
+        self.modelrc = downstream_expert["modelrc"]  # config for model
+        
+        DATA_ROOT = self.datarc['IEMOCAP']
         meta_data = self.datarc["meta_data"]
-
+        
         self.fold = self.datarc.get('test_fold') or kwargs.get("downstream_variant")
         if self.fold is None:
             self.fold = "fold1"
-
-        print(f"[Expert] - using the testing fold: \"{self.fold}\". Ps. Use -o config.downstream_expert.datarc.test_fold=fold2 to change test_fold in config.")
-
-        train_path = os.path.join(
-            meta_data, self.fold.replace('fold', 'Session'), 'train_meta_data.json')
-        print(f'[Expert] - Training path: {train_path}')
-
-        test_path = os.path.join(
-            meta_data, self.fold.replace('fold', 'Session'), 'test_meta_data.json')
-        print(f'[Expert] - Testing path: {test_path}')
         
+        train_path = os.path.join(meta_data, self.fold.replace('fold', 'Session'), 'train_meta_data.json')
+        test_path = os.path.join(meta_data, self.fold.replace('fold', 'Session'), 'test_meta_data.json')
         dataset = IEMOCAPDataset(DATA_ROOT, train_path, self.datarc['pre_load'])
         trainlen = int((1 - self.datarc['valid_ratio']) * len(dataset))
         lengths = [trainlen, len(dataset) - trainlen]
         
         torch.manual_seed(0)
         self.train_dataset, self.dev_dataset = random_split(dataset, lengths)
-
         self.test_dataset = IEMOCAPDataset(DATA_ROOT, test_path, self.datarc['pre_load'])
 
-        model_cls = eval(self.modelrc['select'])
+        Model = eval(self.modelrc['select'])
         model_conf = self.modelrc.get(self.modelrc['select'], {})
-        self.projector = nn.Linear(upstream_dim, self.modelrc['projector_dim'])
-        self.model = model_cls(
-            input_dim = self.modelrc['projector_dim'],
-            output_dim = dataset.class_num,
-            **model_conf,
+        self.connector = nn.Linear(upstream_dim, self.modelrc["input_dim"])
+        self.model = Model(
+            input_dim = self.modelrc['input_dim'],
+            output_class_num=self.train_dataset.class_num, 
+            **self.modelrc
         )
         self.objective = nn.CrossEntropyLoss()
         self.expdir = expdir
-        self.register_buffer('best_score', torch.zeros(1))
+        self.register_buffer("best_score", torch.zeros(1))
 
+    # Interface
+    def get_dataloader(self, split, epoch: int = 0):
+        """
+        Args:
+            split: string
+                'train'
+                    will always be called before the training loop
+                'dev', 'test', or more
+                    defined by the 'eval_dataloaders' field in your downstream config
+                    these will be called before the evaluation loops during the training loop
+        Return:
+            a torch.utils.data.DataLoader returning each batch in the format of:
+            [(wav1,vid1), (wav2,vid2), ...], your_other_contents1, your_other_contents2, ...
+        """
 
-    def get_downstream_name(self):
-        return self.fold.replace('fold', 'emotion')
+        if split == "train":
+            return self._get_train_dataloader(self.train_dataset, epoch)
+        elif split == "dev":
+            return self._get_eval_dataloader(self.dev_dataset)
+        elif split == "test":
+            return self._get_eval_dataloader(self.test_dataset)
 
+    def _get_train_dataloader(self, dataset, epoch: int):
+        from s3prl.utility.data import get_ddp_sampler
 
-    def _get_train_dataloader(self, dataset):
-        sampler = DistributedSampler(dataset) if is_initialized() else None
+        sampler = get_ddp_sampler(dataset, epoch)
         return DataLoader(
-            dataset, batch_size=self.datarc['train_batch_size'],
-            shuffle=(sampler is None), sampler=sampler,
-            num_workers=self.datarc['num_workers'],
-            collate_fn=collate_fn
+            dataset,
+            batch_size=self.datarc["train_batch_size"],
+            shuffle=(sampler is None),
+            sampler=sampler,
+            num_workers=self.datarc["num_workers"],
+            collate_fn=dataset.collate_fn,
         )
 
     def _get_eval_dataloader(self, dataset):
         return DataLoader(
-            dataset, batch_size=self.datarc['eval_batch_size'],
-            shuffle=False, num_workers=self.datarc['num_workers'],
-            collate_fn=collate_fn
+            dataset,
+            batch_size=self.datarc["eval_batch_size"],
+            shuffle=False,
+            num_workers=self.datarc["num_workers"],
+            collate_fn=dataset.collate_fn,
         )
 
-    def get_train_dataloader(self):
-        return self._get_train_dataloader(self.train_dataset)
-
-    def get_dev_dataloader(self):
-        return self._get_eval_dataloader(self.dev_dataset)
-
-    def get_test_dataloader(self):
-        return self._get_eval_dataloader(self.test_dataset)
-
     # Interface
-    def get_dataloader(self, mode):
-        return eval(f'self.get_{mode}_dataloader')()
-
-    # Interface
-    def forward(self, mode, features, labels, filenames, records, **kwargs):
+    def forward(self, split, features, labels, filenames, records, **kwargs):
+        """
+        Args:
+            split: string
+                'train'
+                    when the forward is inside the training loop
+                'dev', 'test' or more
+                    when the forward is inside the evaluation loop
+            features:
+                list of unpadded features [feat1, feat2, ...]
+                each feat is in torch.FloatTensor and already
+                put in the device assigned by command-line args
+            your_other_contents1, ... :
+                in the order defined by your dataloader (dataset + collate_fn)
+                these are all in cpu, and you can move them to the same device
+                as features
+            records:
+                defaultdict(list), by appending contents into records,
+                these contents can be averaged and logged on Tensorboard
+                later by self.log_records (also customized by you)
+                Note1. downstream/runner.py will call self.log_records
+                    1. every `log_step` during training
+                    2. once after evalute the whole dev/test dataloader
+                Note2. `log_step` is defined in your downstream config
+                eg. downstream/example/config.yaml
+        Return:
+            loss:
+                the loss to be optimized, should not be detached
+                a single scalar in torch.FloatTensor
+        """
+        
         device = features[0].device
-        features_len = torch.IntTensor([len(feat) for feat in features]).to(device=device)
-
+        # features_len = torch.IntTensor([len(feat) for feat in features]).to(device=device)
         features = pad_sequence(features, batch_first=True)
-        features = self.projector(features)
-        predicted, _ = self.model(features, features_len)
+        features = self.connector(features)
+        predicted = self.model(features)
 
+        # utterance_labels = your_other_contents1
         labels = torch.LongTensor(labels).to(features.device)
         loss = self.objective(predicted, labels)
 
         predicted_classid = predicted.max(dim=-1).indices
-        records['acc'] += (predicted_classid == labels).view(-1).cpu().float().tolist()
-        records['loss'].append(loss.item())
 
+        records["loss"].append(loss.item())
+        records["acc"] += (predicted_classid == labels).view(-1).cpu().float().tolist()
         records["filename"] += filenames
         records["predict"] += [self.test_dataset.idx2emotion[idx] for idx in predicted_classid.cpu().tolist()]
         records["truth"] += [self.test_dataset.idx2emotion[idx] for idx in labels.cpu().tolist()]
@@ -122,31 +174,57 @@ class DownstreamExpert(nn.Module):
         return loss
 
     # interface
-    def log_records(self, mode, records, logger, global_step, **kwargs):
+    def log_records(self, split, records, logger, global_step, batch_ids, total_batch_num, **kwargs):
+        """
+        Args:
+            split: string
+                'train':
+                    records and batchids contain contents for `log_step` batches
+                    `log_step` is defined in your downstream config
+                    eg. downstream/example/config.yaml
+                'dev', 'test' or more:
+                    records and batchids contain contents for the entire evaluation dataset
+            records:
+                defaultdict(list), contents already prepared by self.forward
+            logger:
+                Tensorboard SummaryWriter
+                please use f'{your_task_name}/{split}-{key}' as key name to log your contents,
+                preventing conflict with the logging of other tasks
+            global_step:
+                The global_step when training, which is helpful for Tensorboard logging
+            batch_ids:
+                The batches contained in records when enumerating over the dataloader
+            total_batch_num:
+                The total amount of batches in the dataloader
+        Return:
+            a list of string
+                Each string is a filename we wish to use to save the current model
+                according to the evaluation result, like the best.ckpt on the dev set
+                You can return nothing or an empty list when no need to save the checkpoint
+        """
         save_names = []
-        for key in ["acc", "loss"]:
-            values = records[key]
+        for key, values in records.items():
             average = torch.FloatTensor(values).mean().item()
             logger.add_scalar(
-                f'emotion-{self.fold}/{mode}-{key}',
+                f'emotion-{self.fold}/{split}-{key}',
                 average,
                 global_step=global_step
             )
             with open(Path(self.expdir) / "log.log", 'a') as f:
                 if key == 'acc':
-                    print(f"{mode} {key}: {average}")
-                    f.write(f'{mode} at step {global_step}: {average}\n')
-                    if mode == 'dev' and average > self.best_score:
+                    print(f"{split} {key}: {average}")
+                    f.write(f'{split} at step {global_step}: {average}\n')
+                    if split == "dev" and average > self.best_score:
                         self.best_score = torch.ones(1) * average
                         f.write(f'New best on {mode} at step {global_step}: {average}\n')
-                        save_names.append(f'{mode}-best.ckpt')
-
-        if mode in ["dev", "test"]:
-            with open(Path(self.expdir) / f"{mode}_{self.fold}_predict.txt", "w") as file:
+                        save_names.append(f"{split}-best.ckpt")
+                        
+        if split in ["dev", "test"]:
+            with open(Path(self.expdir) / f"{split}_{self.fold}_predict.txt", "w") as file:
                 line = [f"{f} {e}\n" for f, e in zip(records["filename"], records["predict"])]
                 file.writelines(line)
 
-            with open(Path(self.expdir) / f"{mode}_{self.fold}_truth.txt", "w") as file:
+            with open(Path(self.expdir) / f"{split}_{self.fold}_truth.txt", "w") as file:
                 line = [f"{f} {e}\n" for f, e in zip(records["filename"], records["truth"])]
                 file.writelines(line)
 
--- downstream_tasks/emotion/model.py
+++ downstream_tasks/emotion/model.py
@@ -1,8 +1,11 @@
-import math
-
+"""
+Small probing model for downstream audio-visual tasks
+Modified from https://github.com/s3prl/s3prl/blob/main/s3prl/downstream/example/model.py
+"""
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
+import math
 
 class SelfAttentionPooling(nn.Module):
     """
@@ -33,60 +36,9 @@ class SelfAttentionPooling(nn.Module):
 
         return utter_rep
 
-
-class CNNSelfAttention(nn.Module):
-    def __init__(
-        self,
-        input_dim,
-        hidden_dim,
-        kernel_size,
-        padding,
-        pooling,
-        dropout,
-        output_class_num,
-        **kwargs
-    ):
-        super(CNNSelfAttention, self).__init__()
-        self.model_seq = nn.Sequential(
-            nn.AvgPool1d(kernel_size, pooling, padding),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(input_dim, hidden_dim, kernel_size, padding=padding),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(hidden_dim, hidden_dim, kernel_size, padding=padding),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(hidden_dim, hidden_dim, kernel_size, padding=padding),
-        )
-        self.pooling = SelfAttentionPooling(hidden_dim)
-        self.out_layer = nn.Sequential(
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, output_class_num),
-        )
-
-    def forward(self, features, att_mask):
-        features = features.transpose(1, 2)
-        features = self.model_seq(features)
-        out = features.transpose(1, 2)
-        out = self.pooling(out, att_mask).squeeze(-1)
-        predicted = self.out_layer(out)
-        return predicted
-
-
-class FCN(nn.Module):
-    def __init__(
-        self,
-        input_dim,
-        hidden_dim,
-        kernel_size,
-        padding,
-        pooling,
-        dropout,
-        output_class_num,
-        **kwargs,
-    ):
-        super(FCN, self).__init__()
+class Model(nn.Module):
+    def __init__(self, input_dim, padding, pooling, dropout, output_class_num, **kwargs):
+        super(Model, self).__init__()
         self.model_seq = nn.Sequential(
             nn.Conv1d(input_dim, 96, 11, stride=4, padding=5),
             nn.LocalResponseNorm(96),
@@ -123,83 +75,3 @@ class FCN(nn.Module):
         out = self.pooling(out).squeeze(-1)
         predicted = self.out_layer(out)
         return predicted
-
-
-class DeepNet(nn.Module):
-    def __init__(
-        self,
-        input_dim,
-        hidden_dim,
-        kernel_size,
-        padding,
-        pooling,
-        dropout,
-        output_class_num,
-        **kwargs,
-    ):
-        super(DeepNet, self).__init__()
-        self.model_seq = nn.Sequential(
-            nn.Conv1d(input_dim, 10, 9),
-            nn.ReLU(),
-            nn.Conv1d(10, 10, 5),
-            nn.ReLU(),
-            nn.Conv1d(10, 10, 3),
-            nn.MaxPool1d(3, 1),
-            nn.BatchNorm1d(10, affine=False),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(10, 40, 3),
-            nn.ReLU(),
-            nn.Conv1d(40, 40, 3),
-            nn.MaxPool1d(2, 1),
-            nn.BatchNorm1d(40, affine=False),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(40, 80, 10),
-            nn.ReLU(),
-            nn.Conv1d(80, 80, 1),
-            nn.MaxPool1d(2, 1),
-            nn.BatchNorm1d(80, affine=False),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(80, 80, 1),
-        )
-        self.pooling = SelfAttentionPooling(80)
-        self.out_layer = nn.Sequential(
-            nn.Linear(80, 30),
-            nn.ReLU(),
-            nn.Linear(30, output_class_num),
-        )
-
-    def forward(self, features, att_mask):
-        features = features.transpose(1, 2)
-        features = self.model_seq(features)
-        out = features.transpose(1, 2)
-        out = self.pooling(out).squeeze(-1)
-        predicted = self.out_layer(out)
-        return predicted
-
-
-class DeepModel(nn.Module):
-    def __init__(
-        self,
-        input_dim,
-        output_dim,
-        model_type,
-        pooling,
-        **kwargs
-    ):
-        super(DeepModel, self).__init__()
-        self.pooling = pooling
-        self.model = eval(model_type)(input_dim=input_dim, output_class_num=output_dim, pooling=pooling, **kwargs)
-
-    def forward(self, features, features_len):
-        attention_mask = [
-            torch.ones(math.ceil((l / self.pooling)))
-            for l in features_len
-        ]
-        attention_mask = pad_sequence(attention_mask, batch_first=True)
-        attention_mask = (1.0 - attention_mask) * -100000.0
-        attention_mask = attention_mask.to(features.device)
-        predicted = self.model(features, attention_mask)
-        return predicted, None
