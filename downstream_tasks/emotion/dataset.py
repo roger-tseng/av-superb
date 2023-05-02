95f4d1739ed2f64ecd423277b166306582a9c8ff
ytc1002 <ytc1002@gmail.com>
2023-05-02 16:45:51 +0800

    filter broken videos

--- downstream_tasks/emotion/dataset.py
+++ downstream_tasks/emotion/dataset.py
@@ -8,10 +8,20 @@ from torch.utils.data import Dataset
 from torchaudio.transforms import Resample
 import imageio
 import cv2
+import torch
 
 
 
 SAMPLE_RATE = 16000
+
+miss = '/data/member1/user_tahsieh/IEMOCAP/miss.txt' 
+miss_list = []
+with open(miss, 'r') as f:
+    line = f.readline()
+    while line:
+        line = f.readline().replace('\n','') #去掉換行
+        miss_list.append(line)
+f.close()
 
 
 class IEMOCAPDataset(Dataset):
@@ -20,15 +30,28 @@ class IEMOCAPDataset(Dataset):
         self.pre_load = pre_load
         with open(meta_path, 'r') as f:
             self.data = json.load(f)
+            
+        index = []
+        for i in range(len(self.data['meta_data'])):
+            path = self.data['meta_data'][i]['path']
+            if path in miss_list:
+                if i not in index:
+                    index.append(i)
+             
+        for i in index[::-1]:
+            del self.data['meta_data'][i]
+ 
         self.class_dict = self.data['labels']
         self.idx2emotion = {value: key for key, value in self.class_dict.items()}
         self.class_num = len(self.class_dict)
         self.meta_data = self.data['meta_data']
+        
         _, origin_sr = torchaudio.load(
             path_join(self.data_dir, self.meta_data[0]['path']))
         self.resampler = Resample(origin_sr, SAMPLE_RATE)
         if self.pre_load:
             self.wavs = self._load_all()
+            
 
     def _load_wav(self, path):
         wav, _ = torchaudio.load(path_join(self.data_dir, path))
@@ -40,16 +63,18 @@ class IEMOCAPDataset(Dataset):
         vid = imageio.get_reader(avi_path, 'ffmpeg')
         capture = cv2.VideoCapture(avi_path)
         frame_count=capture.get(cv2.CAP_PROP_FRAME_COUNT)
-        images_np = []
+        images = []
 
         for frame_num in range(int(frame_count)):
             image = vid.get_data(frame_num)
-            npimage = np.array(image)
-            images_np.append(npimage)
+            image = np.array(image)
+            images.append(image)
 
-        avi_npy = np.stack(images_np)
+        images = np.stack(images)
+        images = torch.from_numpy(images)
+        avi = torch.permute(images, (0, 3, 1, 2))
         
-        return avi_npy
+        return avi
 
     def _load_all(self):
         wavforms = []
@@ -67,7 +92,7 @@ class IEMOCAPDataset(Dataset):
             wav = self.wavs[idx]
         else:
             wav = self._load_wav(self.meta_data[idx]['path'])
-        return wav.numpy(), avi, label, Path(self.meta_data[idx]['path']).stem
+        return wav, avi, label, Path(self.meta_data[idx]['path']).stem
 
     def __len__(self):
         return len(self.meta_data)
