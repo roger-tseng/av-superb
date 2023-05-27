ad47be9b8321ed8a07da5b6673ffec3ee085445a
Ethan-Chiu <ethanchiu0608@gmail.com>
2023-05-27 14:53:49 +0800

    mean pooling

--- downstream_tasks/kinetics_sounds/model.py
+++ downstream_tasks/kinetics_sounds/model.py
@@ -6,27 +6,37 @@ import torch
 import torch.nn as nn
 
 
+# class Model(nn.Module):
+#     def __init__(self, input_dim, hidden_dim, hidden_layers , output_class_num, **kwargs):
+#         super(Model, self).__init__()
+#         self.dropout = 0.35
+
+#         self.lstm = nn.LSTM(input_dim, hidden_dim, hidden_layers, dropout = self.dropout, bidirectional = True, batch_first = True)
+
+#         self.fc = nn.Sequential(
+#             nn.LayerNorm(hidden_dim * 2),
+#             nn.ReLU(),
+#             nn.Dropout(self.dropout),
+#             nn.Linear(hidden_dim * 2, hidden_dim),
+#             nn.LayerNorm(hidden_dim),
+#             nn.ReLU(),
+#             nn.Dropout(self.dropout),
+#             nn.Linear(hidden_dim, output_class_num)
+#         )
+
+#     def forward(self, features):
+#         out, _ = self.lstm(features)
+#         out = out[:, -1, :]
+#         predicted = self.fc(out)
+#         return predicted
+    
 class Model(nn.Module):
-    def __init__(self, input_dim, hidden_dim, hidden_layers , output_class_num, **kwargs):
+    def __init__(self, input_dim, output_class_num, **kwargs):
         super(Model, self).__init__()
-        self.dropout = 0.35
-
-        self.lstm = nn.LSTM(input_dim, hidden_dim, hidden_layers, dropout = self.dropout, bidirectional = True, batch_first = True)
-
-        self.fc = nn.Sequential(
-            nn.LayerNorm(hidden_dim * 2),
-            nn.ReLU(),
-            nn.Dropout(self.dropout),
-            nn.Linear(hidden_dim * 2, hidden_dim),
-            nn.LayerNorm(hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(self.dropout),
-            nn.Linear(hidden_dim, output_class_num)
-        )
+        self.linear = nn.Linear(input_dim, output_class_num)
 
     def forward(self, features):
-        out, _ = self.lstm(features)
-        out = out[:, -1, :]
-        predicted = self.fc(out)
+        pooled = features.mean(dim=1)
+        predicted = self.linear(pooled)
         return predicted
 
