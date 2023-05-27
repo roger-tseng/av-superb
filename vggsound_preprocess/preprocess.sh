cfa53b9433710411d690c697ff54b3b8738c14c5
u2707828 <u2707828@ln01-twnia2.twcc.ai>
2023-05-27 13:54:46 +0800

    rewrite preprocess

--- vggsound_preprocess/preprocess.sh
+++ vggsound_preprocess/preprocess.sh
@@ -1,7 +1,7 @@
 rm -r ../split/
 mkdir ../split/
 mkdir ../split/ref/
-mkdir ../split/data/
+mkdir ../split/split/
 
 echo "Get vggsound dataset csv file"
 wget -O ../split/ref/vggsound.csv https://raw.githubusercontent.com/hche11/VGGSound/master/data/vggsound.csv
@@ -10,4 +10,4 @@ echo "Changing labels to int labels"
 python3 change_label.py --csv ../split/ref/vggsound.csv --output_dir ../split/ref --output_filename vggsound_int_labels.csv
 
 echo "Checking existing video datasets"
-python3 check_dataset.py --csv ../split/ref/vggsound_int_labels.csv --dest ../split/data --data /work/u2707828/VGGSound/VGGSound/ --reduce_class 310
+python3 check_dataset.py --csv ../split/ref/vggsound_int_labels.csv --dest ../split/split --data /work/u2707828/VGGSound/VGGSound/ --reduce_class 310
