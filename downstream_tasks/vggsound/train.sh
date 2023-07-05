8b7b45ca62025e15f55b0305de9f75ba7cbd35ac
u2707828 <u2707828@ln01-twnia2.twcc.ai>
2023-07-05 14:37:36 +0800

    change configuration

--- downstream_tasks/vggsound/train.sh
+++ downstream_tasks/vggsound/train.sh
@@ -3,13 +3,20 @@ cd ../../
 # python3 run_downstream.py -m train -e ./result/downstream/avbert_vggsound_fusion_lr1E03/states-50.ckpt
 
 # python3 run_downstream.py -m train -n avhubert_vggsound_fusion_lr1E04 -u avhubert -d vggsound -s fusion_feats
-# python3 run_downstream.py -m train -n mavilbase_vggsound_video_lr1E05 -u mavil_base -d vggsound -s video_feats --pooled_features_path /work/u2707828/features/
 
 
-# python3 run_downstream.py -m train -n testreplaitest -u replai -d vggsound -s video_feats --pooled_features_path /work/u2707828/features/
+# python3 run_downstream.py -m train -n avhubert_ft_lrs3_433_vggsound_fusion_lr1E02 -u avhubert_ft_lrs3_433 -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
 
-# python3 run_downstream.py -m train -n testmavilbasetest -u mavil_base -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
+python3 run_downstream.py -m train -n avhubert_vggsound_video_lr1E05 -u avhubert -d vggsound -s video_feats --pooled_features_path /work/u2707828/features/
 
-# python3 run_downstream.py -m train -n testavberttest1234582 -u avbert -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
 
-python3 run_downstream.py -m train -n testhuberttest1234582 -u hubert -d vggsound -s audio_feats --pooled_features_path /work/u2707828/features/
+# python3 run_downstream.py -m train -n mavil_vggsound_fusion_lr1E02 -u mavil_base -k /work/u2707828/mavil_as_pt_ft_a+v.pth -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
+
+
+# python3 run_downstream.py -m train -n testhubert -u hubert -d vggsound -s audio_feats --pooled_features_path /work/u2707828/features/
+
+# python3 run_downstream.py -m train -n testavhubert -u avhubert -d vggsound -s audio_feats --pooled_features_path /work/u2707828/features/
+
+# python3 run_downstream.py -m train -n testavhubert_large_lrs3 -u avhubert_large_lrs3 -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
+
+
