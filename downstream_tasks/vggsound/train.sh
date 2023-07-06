5b11a9e713751f18c35b39e0da674c250a69f489
u2707828 <u2707828@ln01-twnia2.twcc.ai>
2023-07-06 14:19:19 +0800

    add download previous result loadresult.sh

--- downstream_tasks/vggsound/train.sh
+++ downstream_tasks/vggsound/train.sh
@@ -7,7 +7,7 @@ cd ../../
 
 # python3 run_downstream.py -m train -n avhubert_ft_lrs3_433_vggsound_fusion_lr1E02 -u avhubert_ft_lrs3_433 -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
 
-python3 run_downstream.py -m train -n avhubert_vggsound_video_lr1E05 -u avhubert -d vggsound -s video_feats --pooled_features_path /work/u2707828/features/
+# python3 run_downstream.py -m train -n avhubert_vggsound_video_lr1E05 -u avhubert -d vggsound -s video_feats --pooled_features_path /work/u2707828/features/
 
 
 # python3 run_downstream.py -m train -n mavil_vggsound_fusion_lr1E02 -u mavil_base -k /work/u2707828/mavil_as_pt_ft_a+v.pth -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
@@ -19,4 +19,7 @@ python3 run_downstream.py -m train -n avhubert_vggsound_video_lr1E05 -u avhubert
 
 # python3 run_downstream.py -m train -n testavhubert_large_lrs3 -u avhubert_large_lrs3 -d vggsound -s fusion_feats --pooled_features_path /work/u2707828/features/
 
-
+gdown --fuzzy https://drive.google.com/file/d/1L9aM0S2_Nrbk-LPrq_K36VhZQR_53_Rl/view?usp=sharing
+mkdir ../../result/vggsound/
+unzip result.zip -d ../../result/vggsound/
+rm result.zip
---
+++ downstream_tasks/vggsound/vggsound_preprocess/loadresult.sh
@@ -0,0 +1,3 @@
+gdown --fuzzy https://drive.google.com/file/d/1L9aM0S2_Nrbk-LPrq_K36VhZQR_53_Rl/view?usp=sharing
+mkdir ../../result/vggsound
+unzip result.zip -d ../../result/vggsound/
