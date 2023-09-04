b0efbdb25211a83f277d84c6d7d0ab466eca6531
max20301 <b09901052@ntu.edu.tw>
2023-09-04 21:53:04 +0800

    fix hog

--- upstream_models/hog/expert.py
+++ upstream_models/hog/expert.py
@@ -19,13 +19,26 @@ class UpstreamExpert(UpstreamBase):
     def __init__(self, model_config, **kwargs):
         super().__init__(**kwargs)
 
+        self.video_frame_size = (128, 64)
         self.video_frame_rate = 25
 
         with open(model_config, "r") as file:
             self.config = yaml.load(file, Loader=yaml.FullLoader)
 
     def preprocess_video(self, video, video_frame_rate):
+        
+        # Resize video frames
+        video_frames = []
+        for frame in video:
+            video_frames.append(
+                torchvision.transforms.functional.resize(
+                    frame, self.video_frame_size, antialias=False
+                )
+            )
+        video = torch.stack(video_frames)
 
+        # Resample video
+        # (from https://github.com/pytorch/vision/blob/5b07d6c9c6c14cf88fc545415d63021456874744/torchvision/datasets/video_utils.py#L278)
         step = float(video_frame_rate) / self.video_frame_rate
         if step.is_integer():
             # optimization: if step is integer, don't need to perform
@@ -37,6 +50,11 @@ class UpstreamExpert(UpstreamBase):
             idxs = torch.arange(num_frames, dtype=torch.float32) * step
             idxs = idxs.floor().to(torch.int64)
         video = video[idxs]
+
+        if video.shape[1] == 3:
+            video = 0.2989 * video[:, 0] + 0.587 * video[:, 1] + 0.114 * video[:, 2]
+        else:
+            video = video.mean(dim=1)
 
         return video
     
@@ -61,22 +79,17 @@ class UpstreamExpert(UpstreamBase):
         #
 
         videos = pad_sequence(video, batch_first = True)
-        videos = videos.permute(0, 1, 3, 4, 2)
-
-        print(videos.shape)
 
         video_feats = []
         for video in videos:
             video_feat = []
             for frame in video:
-                feat = hog(frame.cpu(), orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), channel_axis=-1)
+                feat = hog(frame.cpu(), orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1))
                 feat = torch.from_numpy(feat)
                 video_feat.append(feat)
             video_feat = torch.stack(video_feat)
             video_feats.append(video_feat)
-        video_feats = torch.stack(video_feats)
-
-        print(video_feats.shape)
+        video_feats = torch.stack(video_feats).cuda()
 
         return {
             "video_feats": [video_feats],
